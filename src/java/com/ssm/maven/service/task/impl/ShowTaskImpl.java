eafc12cf657b4783772ce520ab1821500ff53acb
宋益民 <song.yimin@cesgroup.com.cn>
2017-10-30 18:33:10 +0800

    dsrw

--- src/java/com/ssm/maven/service/task/impl/ShowTaskImpl.java
+++ src/java/com/ssm/maven/service/task/impl/ShowTaskImpl.java
@@ -1,6 +1,10 @@
 package com.ssm.maven.service.task.impl;
 
+import com.ssm.maven.dao.BookDao;
 import com.ssm.maven.service.task.ShowTask;
+import com.ssm.maven.util.ScheduleUtils;
+import com.ssm.maven.util.ScheduleUtils.Job;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,16 +20,33 @@ import java.util.Date;
 @Component
 public class ShowTaskImpl implements ShowTask {
     DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-    @Scheduled(cron ="0/1 * *  * * ? ")
+    @Autowired
+    private BookDao dao;
+    @Scheduled(cron = "0/10 * *  * * ? ")
     @Override
     public void showA() {
+
         System.out.println(sdf.format(new Date())+"*********A任务每1秒执行一次进入测试");
+        Job job1 = new Job();
+        job1.setClassName("com.ssm.maven.service.task.impl.ShowTaskImpl");
+        job1.setCron("0/1 * * * * ?");
+        job1.setJobName("定时器1");
+        job1.setMethodName("showB");
+        job1.setStatus(1);
+
+
+        try {
+            ScheduleUtils.add(job1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 
-    @Scheduled(cron ="0/5 * *  * * ? ")
+    /*@Scheduled(cron = "0/1 * *  * * ? ")*/
     @Override
     public void showB() {
-        System.out.println(sdf.format(new Date())+"*********b任务每5秒执行一次进入测试");
+        System.out.println(sdf.format(new Date()) + "*********b任务每5秒执行一次进入测试");
     }
 }
